8244b7cb387c4d7fe44c676017393ba12e70352d
Alfredo Quintero Tlacuilo <alfredoq98@hotmail.com>
2019-08-14 12:52:22 -0500

    Added source code

---
+++ README.md
@@ -0,0 +1,11 @@
+# change-stream-to-async-iterator
+
+[![npm version](https://badge.fury.io/js/change-stream-to-async-iterator.svg)](https://badge.fury.io/js/change-stream-to-async-iterator)
+
+Turn any [MongoDB ChangeStream](https://docs.mongodb.com/manual/changeStreams/) into an async iterator.
+
+GraphQL subscriptions expect to be passed an async iterator. Now you can turn any MongoDB ChangeStream into one. I hope it's useful for you!
+
+## License
+
+Licensed under the MIT License, Copyright copyright 2019 Maximilian Stoiber. See [LICENSE](https://github.com/alfredoqt/change-stream-to-async-iterator/blob/master/LICENSE) for more information.
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,47 @@
-function greeter(person: string) {
-  return 'Hello, ' + person;
+import {
+  ChangeStreamOptions,
+  ClientSession,
+  Collection,
+  Timestamp
+} from 'mongodb';
+
+function changeStreamToAsyncIterator(
+  collection: Collection,
+  pipeline?: object[],
+  options?: ChangeStreamOptions & {
+    startAtOperationTime?: Timestamp;
+    session?: ClientSession;
+  }
+): AsyncIterator<any> {
+  const pipelineArg = pipeline || [];
+  const optionsArg = options || {};
+
+  const changeStream = collection.watch(pipelineArg, optionsArg);
+
+  return {
+    next(): Promise<IteratorResult<any>> {
+      if (changeStream.isClosed()) {
+        return this.return();
+      }
+      return changeStream.next().then(data => {
+        return { value: data, done: false };
+      });
+    },
+    return(): Promise<IteratorResult<any>> {
+      if (changeStream.isClosed()) {
+        return Promise.resolve({ value: undefined, done: true });
+      }
+      return changeStream
+        .close()
+        .then(() => ({ value: undefined, done: true }));
+    },
+    throw(error: Error): Promise<IteratorResult<any>> {
+      if (!changeStream.isClosed()) {
+        changeStream.close();
+      }
+      return Promise.reject({ value: error, done: true });
+    }
+  };
 }
+
+export default changeStreamToAsyncIterator;
